341ef20a7025c14fac5c81d4145afdbc21661ba9
Miguel Charris <miguelangelcharriscarmona@gmail.com>
2025-05-01 16:37:09 -0500

    feat [triangles] - Se dibujan dos triangulos de diferente color usando diferentes Fragment Shaders - Se optimiza el uso de VAO's y VBO's

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,18 @@ auto vertexShaderSource = "#version 330 core\n"
         "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
         "}\0";
 
-auto fragmentShaderSource = "#version 330 core\n"
+auto fragmentShaderSourceBlue = "#version 330 core\n"
         "out vec4 FragColor;\n"
         "void main()\n"
         "{\n"
-        "   FragColor = vec4(0.3f, 0.15f, 0.8f, 1.0f);\n"
+        "   FragColor = vec4(0.0f, 0.0f, 1.0f, 1.0f);\n"
+        "}\n\0";
+
+auto fragmentShaderSourceYellow = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
         "}\n\0";
 
 int main() {
@@ -68,28 +75,51 @@ int main() {
         std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
 
-    // Creacion de Fragment Shader
-    unsigned int fragmentShader{glCreateShader(GL_FRAGMENT_SHADER)};
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragmentShader);
+    // Creacion de Fragment Shader Blue
+    unsigned int fragmentShaderBlue{glCreateShader(GL_FRAGMENT_SHADER)};
+    glShaderSource(fragmentShaderBlue, 1, &fragmentShaderSourceBlue, nullptr);
+    glCompileShader(fragmentShaderBlue);
 
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    glGetShaderiv(fragmentShaderBlue, GL_COMPILE_STATUS, &success);
     if (!success) {
-        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        glGetShaderInfoLog(fragmentShaderBlue, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::BLUE::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
 
-    unsigned int shaderProgram{glCreateProgram()};
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    // Creacion de Fragment Shader Yellow
+    unsigned int fragmentShaderYellow{glCreateShader(GL_FRAGMENT_SHADER)};
+    glShaderSource(fragmentShaderYellow, 1, &fragmentShaderSourceYellow, nullptr);
+    glCompileShader(fragmentShaderYellow);
+
+    glGetShaderiv(fragmentShaderYellow, GL_COMPILE_STATUS, &success);
     if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        glGetShaderInfoLog(fragmentShaderYellow, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::YELLOW::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
+
+    unsigned int shaderProgramBlue{glCreateProgram()};
+    glAttachShader(shaderProgramBlue, vertexShader);
+    glAttachShader(shaderProgramBlue, fragmentShaderBlue);
+    glLinkProgram(shaderProgramBlue);
+    glGetProgramiv(shaderProgramBlue, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgramBlue, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM:BLUE::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    unsigned int shaderProgramYellow{glCreateProgram()};
+    glAttachShader(shaderProgramYellow, vertexShader);
+    glAttachShader(shaderProgramYellow, fragmentShaderYellow);
+    glLinkProgram(shaderProgramYellow);
+    glGetProgramiv(shaderProgramYellow, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgramYellow, 512, nullptr, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::YELLOW::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
     glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    glDeleteShader(fragmentShaderBlue);
+    glDeleteShader(fragmentShaderYellow);
 
     float triangle1Vertices[] = {
         0.0f, 0.0f, 0.0f,
@@ -104,53 +134,38 @@ int main() {
     };
 
     // Creacion de Vertex Array Object y Vertex Buffer Object para triangulo 1
-    unsigned int VAO1{};
-    unsigned int VBO1{};
-    glGenVertexArrays(1, &VAO1);
-    glGenBuffers(1, &VBO1);
-
-    // Creacion de Vertex Array Object y Vertex Buffer Object para triangulo 2
-    unsigned int VAO2{};
-    unsigned int VBO2{};
-    glGenVertexArrays(1, &VAO2);
-    glGenBuffers(1, &VBO2);
+    unsigned int VAO1[2]{};
+    unsigned int VBO1[2]{};
+    glGenVertexArrays(2, VAO1);
+    glGenBuffers(2, VBO1);
 
     // Viculamos el VAO del triangulo 1
-    glBindVertexArray(VAO1);
+    glBindVertexArray(VAO1[0]);
 
     // Configuramos el VBO del triangulo 1
 
     // Asignar vertices al VBO1 (llenar el buffer con los datos del triangulo 1)
-    glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO1[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(triangle1Vertices), triangle1Vertices, GL_STATIC_DRAW);
 
     // Configurar los atributos del VBO del triangulo 1
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void *>(nullptr));
     glEnableVertexAttribArray(0);
 
-    // Desvincular el VBO del triangulo 1
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // Desvincular el VAO del triangulo 1
-    glBindVertexArray(0);
-
     // Viculamos el VAO del triangulo 2
-    glBindVertexArray(VAO2);
+    glBindVertexArray(VAO1[1]);
 
     // Configuramos el VBO del triangulo 2
 
     // Asignar vertices al VBO1 (llenar el buffer con los datos del triangulo 2)
-    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO1[1]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(triangle2Vertices), triangle2Vertices, GL_STATIC_DRAW);
 
     // Configurar los atributos del VBO del triangulo 2
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void *>(nullptr));
+    // NOTA: Cuando la separacion entre atributos es 0, es decir estan empaquetados, el stride puede ser 0
+    // Y dejar que openGL lo interprete
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, static_cast<void *>(nullptr));
     glEnableVertexAttribArray(0);
-
-    // Desvincular el VBO del triangulo 2
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // Desvincular el VAO del triangulo 2
     glBindVertexArray(0);
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
@@ -162,20 +177,21 @@ int main() {
         glClearColor(0.63f, 0.50f, 0.50f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO1);
+        glUseProgram(shaderProgramBlue);
+        glBindVertexArray(VAO1[0]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
-        glBindVertexArray(0);
-        glBindVertexArray(VAO2);
+
+        glUseProgram(shaderProgramYellow);
+        glBindVertexArray(VAO1[1]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1, &VAO1);
-    glDeleteBuffers(1, &VBO1);
-    glDeleteProgram(shaderProgram);
+    glDeleteVertexArrays(2, VAO1);
+    glDeleteBuffers(2, VBO1);
+    glDeleteProgram(shaderProgramBlue);
 
     glfwTerminate();
     return 0;
